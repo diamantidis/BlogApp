8c69b9ef7d856c3fb26c1d7eda0fb3c0f4ebae9b
Ioannis Diamantidis <diamantidis@outlook.com>
2019-10-12 10:46:03 +0200

    Added iOS app

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -5,6 +5,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        vc.view.backgroundColor = .white
+        let navController = UINavigationController(rootViewController: vc)
+        window!.rootViewController = navController
+        window!.makeKeyAndVisible()
         return true
     }
 
--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -1,14 +1,67 @@
 import UIKit
 import shared
 
-class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        label.text = Proxy().proxyHello()
+class ViewController: UIViewController, FeedView {
+
+    func showError(error: KotlinThrowable) {
+        activityIndicatorView.stopAnimating()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func showData(feed: Feed) {
+        activityIndicatorView.stopAnimating()
+        self.tableView.separatorStyle = .singleLine
+        self.posts = feed.items
+        self.tableView.reloadData()
     }
-    @IBOutlet weak var label: UILabel!
+
+    func showLoading() {
+        self.tableView.separatorStyle = .none
+        activityIndicatorView.startAnimating()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+
+        tableView.backgroundView = activityIndicatorView
+        presenter.loadData()
+    }
+
+    private var posts = [Item]()
+    private lazy var presenter = PresenterFactory.Companion().createFeedPresenter(view: self)
+    private lazy var activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+
+    private lazy var tableView: UITableView = {
+        var tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return posts.isEmpty ? 0 : 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel!.text = posts[indexPath.row].title
+        return cell
+    }
 }
